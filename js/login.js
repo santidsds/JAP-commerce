9c8bdae3d3fb017ee8f278e26b0856e292c01ce8
Santiago <santidasilva1408@gmail.com>
2022-08-11 23:01:33 -0300

    login page

---
+++ js/login.js
@@ -0,0 +1,27 @@
+
+let loginBtn = document.getElementById("login-btn")
+let passAlertp = document.getElementById("pass-alert")
+let emailAlertp = document.getElementById("email-alert")
+let emailInput = document.getElementById("email")
+let passInput = document.getElementById("pass")
+
+loginBtn.addEventListener("click", () => {
+    console.log(passInput.value.length)
+    
+    if(emailInput.value.length === 0 || passInput.value.length === 0){
+       passAlertp.innerHTML = "Ningún campo no puede estar vacío"
+    } 
+    else if(emailInput.value.length < 6 || passInput.value.length < 6){
+        passAlertp.innerHTML = "La contraseña y el email deben ser mayor a 6 caracteres"
+    }
+    else {
+        passAlertp.innerHTML = ""
+        window.location.replace("index.html");
+
+    }
+        
+    
+    
+    
+})
+
