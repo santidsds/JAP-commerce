5f1d4fa922c00c6b3e23c0b5aaf0d904852b3031
Santiago <santidasilva1408@gmail.com>
2022-10-14 18:53:10 -0300

    show cart items

--- js/cart.js
+++ js/cart.js
@@ -21,3 +21,96 @@ document.getElementById("user-settings-cart").addEventListener("click", () => {
 document.getElementById("user-settings-perfil").addEventListener("click", () => {
   window.location.replace("my-profile.html")
 })
+
+let id = 25801;
+let cartArticlesURL = CART_INFO_URL + id + EXT_TYPE
+let cartArticlesArray = []
+
+let cartItemsCont = document.getElementById("cart-items-cont");
+let cartSubtotalItemsCont = document.getElementById("subtotal-article");
+let subtotal = document.getElementById("subtotal");
+
+window.addEventListener("DOMContentLoaded", () => {
+  getJSONData(cartArticlesURL).then((res) => {
+    if(res.status === "ok"){
+      cartArticlesArray = res.data
+      showCartItems()
+      
+    }
+    
+  })
+  
+  
+})
+
+function showCartItems () {
+  for(let i=0; i<cartArticlesArray.articles.length; i++){
+    let subtotalCount = 0
+    let item = cartArticlesArray.articles[0]
+    
+
+    cartItemsCont.innerHTML += `
+    <div class="left-cont-cart">
+    <img src=${item.image} alt="">
+    <div class="about">
+      <h1 class="title">${item.name}</h1>
+      <div class="counter">
+        <div id="cartCounterBtnMenos${i}" class="btn">-</div>
+        <div id="cartCounterResult${i}" class="count">1</div>
+        <div id="cartCounterBtnMas${i}" class="btn">+</div>
+      </div>
+      
+    </div>
+  </div>
+  
+
+  <div class="prices">
+    <p id="cartPrice">${item.currency} ${item.unitCost}</p>
+    <button id="cartRemoveItem" class="cartRemoveItem">Remove</button>
+  </div>
+    `
+
+    
+  let itemCounter = parseInt(document.getElementById("cartCounterResult"+i).innerHTML);
+  let counter = 0
+  let itemCounterMas = document.getElementById("cartCounterBtnMas"+i);
+  let itemCounterMenos = document.getElementById("cartCounterBtnMenos"+i);
+  let subtotalItemCount = document.getElementById("itemCount");
+  
+  subtotalCount = item.unitCost
+    
+    itemCounterMas.addEventListener("click", () => {
+      counter += 1
+      
+      document.getElementById("cartCounterResult"+i).innerHTML = itemCounter + counter
+
+      let currentItemCounter = parseInt(document.getElementById("cartCounterResult"+i).innerHTML);
+
+      subtotal.innerHTML = `
+        ${item.currency} ${subtotalCount * currentItemCounter}
+      `
+      subtotalItemCount.innerHTML = `
+        ${currentItemCounter} items
+      `
+    })
+  
+    itemCounterMenos.addEventListener("click", () => {
+      counter -= 1
+
+      document.getElementById("cartCounterResult"+i).innerHTML = itemCounter + counter
+
+      let currentItemCounter = parseInt(document.getElementById("cartCounterResult"+i).innerHTML);
+
+      subtotal.innerHTML = `
+        ${item.currency} ${subtotalCount * currentItemCounter}
+      `
+      subtotalItemCount.innerHTML = `
+        ${currentItemCounter} items
+      `
+    })
+  
+  subtotal.innerHTML = `
+    ${item.currency} ${subtotalCount}
+  `
+  }
+}
